0d06542e57d9ffe2f5836a73daded6495ea95e8d
Aijun Nie <naj@sidri.com>
2017-10-12 17:08:50 +0800

    测试

---
+++ SportsStoreNaj.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStoreNaj.Domain.Abstract;
+using SportsStoreNaj.Domain.Entities;
+namespace SportsStoreNaj.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+        public ProductController(IProductsRepository productsRepository)
+        {
+            repository = productsRepository;
+        }
+
+        // GET: Product
+        public ActionResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
--- SportsStoreNaj.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStoreNaj.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Ninject;
-
+using Moq;
+using SportsStoreNaj.Domain.Abstract;
+using SportsStoreNaj.Domain.Entities;
 namespace SportsStoreNaj.WebUI.Infrastructure
 {
     public class NinjectDependencyResolver : IDependencyResolver
@@ -18,6 +20,15 @@ namespace SportsStoreNaj.WebUI.Infrastructure
         private void AddBindings()
         {
             //绑定代码
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                new Product{ Name="FootBall",Price=25},
+                new Product{ Name="Surf Board",Price=179},
+                new Product{ Name="Running Shoes",Price=95},
+                new Product{ Name="T-Shirt",Price=35.75m}
+            });
+
+            _Kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
         }
 
         public object GetService(Type serviceType)
