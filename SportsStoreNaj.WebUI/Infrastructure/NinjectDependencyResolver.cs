d70dcf7b09e6ee1cd9f5a2090d91979f877ad78d
unknown <naj@FileServer.com>
2017-10-12 14:33:23 +0800

    添加项目文件。

---
+++ SportsStoreNaj.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStoreNaj.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _Kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _Kernel = kernel;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            //绑定代码
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _Kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _Kernel.GetAll(serviceType);
+        }
+    }
+}
